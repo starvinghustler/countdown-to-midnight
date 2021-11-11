7ba5a44728883265bff47b2d2a68874a7bc37475
idobok <idobok12@gmail.com>
2021-11-11 12:46:31 +0200

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+    while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        sleep(1) #not sure what this does but basically enables the 5 second runoff that spec needed to pass test
+        seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+end    
